d9797624bd26aa6069ed35799109b94328afdc4f
Rohittyagiii <tyagirohit698@gmail.com>
2026-06-25 16:03:21 +0530

    goaldata

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
  import React from "react";
  import GoalList from "./components/GoalList"
+ import NewGoal from "./components/NewGoal";
  import "./App.css";
-
 
 function App() { 
   const courseGoals = [
@@ -9,9 +9,16 @@ function App() {
     {id:'cg2', text:'Learn all about the Course Main Topic'},
     {id:'cg3', text:'Help other students in the Course Q&A'},
   ]
+   
+    const addNewGaolHandler = (newGoal) =>{
+        courseGoals.push(newGoal);
+        console.log(courseGoals);
+        
+    }
   return (
   <div className="course-goals"> 
     <h2>Course Goals</h2>
+    <NewGoal onAddGoal={addNewGaolHandler}/>
     <GoalList goals={courseGoals}/>
   </div>
 );
